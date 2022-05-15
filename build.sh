15e8eeff68abdbdfddb9f60e3388bd02eeb3d6bd
Joe Cooper <swelljoe@gmail.com>
2022-05-15 18:04:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,41 +1,31 @@
 #!/bin/sh
 # builds Pro/GPL release packages
-rpmbuild --define "_topdir $(pwd)/rpmbuild" -bb rpmbuild/SPECS/virtualmin-release.spec
+rpmbuild --define "_topdir $(pwd)/rpmbuild" -bb rpmbuild/SPECS/virtualmin-pro-release.spec
 rpmbuild --define "_topdir $(pwd)/rpmbuild" -bb rpmbuild/SPECS/virtualmin-gpl-release.spec
 
-signed=0
-while [ $signed -eq 0 ]; do
-	rpmsign -D '_gpg_name Virtualmin, Inc. (Package signing key for Virtualmin 6) <security@virtualmin.com>' --addsign rpmbuild/RPMS/noarch/*.rpm
-	if [ $? -eq 0 ]; then
-		signed=1
-	fi
-done
+rpmsign -D '_gpg_name Virtualmin, Inc. (Package signing key for Virtualmin 7) <security@virtualmin.com>' --addsign rpmbuild/RPMS/noarch/*.rpm
 
 cwd=$(pwd)
-gplrel=$(find . -name *gpl.noarch.rpm)
-echo "Copying $gplrel to GPL repositories..."
-cp $gplrel $HOME/result/vm/6/gpl/centos/6/i386
-cp $gplrel $HOME/result/vm/6/gpl/centos/6/x86_64
-cp $gplrel $HOME/result/vm/6/gpl/centos/7/x86_64
-for i in centos/6/i386 centos/6/x86_64 centos/7/x86_64; do
-	echo "Adding link to $HOME/result/vm/6/gpl/$i..."
-	cd $HOME/result/vm/6/gpl/$i
-	rm virtualmin-release-latest.noarch.rpm
-	ln -s $(basename $gplrel) virtualmin-release-latest.noarch.rpm
-done
-cd $cwd
-rm $gplrel
+# GPL
+gplrel=$(find . -name 'virtualmin-gpl-release*.noarch.rpm')
+echo "Copying to repository..."
+cp "$gplrel" "$HOME/result/vm/7/rpm/"
+echo "Adding links..."
+cd "$HOME/result/vm/7/rpm/" || exit 1
+rm virtualmin-gpl-release.noarch.rpm
+gplpackage=$(basename "$gplrel")
+ln -s "$gplpackage" virtualmin-gpl-release.noarch.rpm
+cd "$cwd" || exit 1
+rm "$gplrel"
 
-prorel=$(find . -name *.noarch.rpm)
-echo "Copying $prorel to Pro repositories..."
-cp $prorel $HOME/result/vm/6/centos/6/i386
-cp $prorel $HOME/result/vm/6/centos/6/x86_64
-cp $prorel $HOME/result/vm/6/centos/7/x86_64
-for i in centos/6/i386 centos/6/x86_64 centos/7/x86_64; do
-	echo "Adding link to $HOME/result/vm/6/$i..."
-        cd $HOME/result/vm/6/$i
-        rm virtualmin-release-latest.noarch.rpm
-        ln -s $(basename $prorel) virtualmin-release-latest.noarch.rpm
-done
-cd $cwd
-#rm $prorel
+# Pro
+prorel=$(find . -name 'virtualmin-pro-release*.noarch.rpm')
+cp "$prorel" "$HOME/result/vm/7/rpm/"
+echo "Adding links..."
+cd "$HOME/result/vm/7/rpm/" || exit 1
+rm virtualmin-pro-release.noarch.rpm
+propackage=$(basename "$prorel")
+ln -s "$propackage" virtualmin-pro-release.noarch.rpm
+cd "$cwd" || exit 1 
+rm "$prorel"
+
